8653d52018aa7c0dbcad93dac2f203950efe4b31
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2024-04-02 22:39:31 +0200

    added slight padding to the search radio button(s)

--- src/entries/popup/routes/Search.svelte
+++ src/entries/popup/routes/Search.svelte
@@ -7,7 +7,11 @@
   import Icon from "svelte-fa";
   import { faSpinner } from "@fortawesome/free-solid-svg-icons";
   import { getContextClient, queryStore } from "@urql/svelte";
-  import { SearchMediaDocument, MediaType, MediaFormat } from "@anilist/graphql";
+  import {
+    SearchMediaDocument,
+    MediaType,
+    MediaFormat,
+  } from "@anilist/graphql";
   import { debounce } from "$lib/util";
   import SearchResults from "$lib/components/SearchResults.svelte";
   import Loader from "$lib/components/Loader.svelte";
@@ -22,13 +26,25 @@
     query: SearchMediaDocument,
     variables: {
       search: searchText,
-      type: selectedMode === "ALL" ? undefined : selectedMode === "ANIME" ? MediaType.ANIME : MediaType.MANGA,
-      format: selectedMode === "ALL" ? undefined : selectedMode === "MANGA" ? [ MediaFormat.MANGA, MediaFormat.ONE_SHOT ] : selectedMode === "LIGHT_NOVEL" ? [ MediaFormat.NOVEL ] : undefined,
+      type:
+        selectedMode === "ALL"
+          ? undefined
+          : selectedMode === "ANIME"
+            ? MediaType.ANIME
+            : MediaType.MANGA,
+      format:
+        selectedMode === "ALL"
+          ? undefined
+          : selectedMode === "MANGA"
+            ? [MediaFormat.MANGA, MediaFormat.ONE_SHOT]
+            : selectedMode === "LIGHT_NOVEL"
+              ? [MediaFormat.NOVEL]
+              : undefined,
     },
-    pause: searchText === ""
+    pause: searchText === "",
   });
-  const debounceSearchText = debounce<(text: string) => void>(text => {
-    searchText = text
+  const debounceSearchText = debounce<(text: string) => void>((text) => {
+    searchText = text;
   }, 700);
 
   onMount(() => textField?.focus());
@@ -36,38 +52,42 @@
 
 <div class="w-full h-full flex flex-col items-center">
   <div class="relative w-2/3">
-    <input 
-      type="text" 
-      placeholder="Search AniList..." 
+    <input
+      type="text"
+      placeholder="Search AniList..."
       class="w-full p-2 mt-4 rounded-md text-md bg-foreground focus:outline-none focus:ring-2 ring-accent"
       bind:this={textField}
-      on:input={e => debounceSearchText(e.currentTarget.value)}
+      on:input={(e) => debounceSearchText(e.currentTarget.value)}
     />
     {#if $search.fetching}
-      <Icon class="absolute right-2 bottom-3 pointer-events-none text-accent" icon={faSpinner} spin />
+      <Icon
+        class="absolute right-2 bottom-3 pointer-events-none text-accent"
+        icon={faSpinner}
+        spin
+      />
     {/if}
   </div>
-  <div class="w-2/3 mt-4 grid grid-cols-4 border border-text-300 rounded-md">
-    <button 
-      class="px-2 py-0.5 text-sm rounded-md hover:bg-accent/40 hover:text-text-400 transition-colors" 
+  <div class="w-2/3 mt-2 grid grid-cols-4 rounded-lg p-1 gap-0.5 bg-foreground">
+    <button
+      class="px-2 py-0.5 text-sm rounded hover:bg-accent/40 hover:text-text-400 transition-colors"
       class:selected={selectedMode === "ALL"}
-      on:click={() => selectedMode = "ALL"}
-    >All</button>
-    <button 
-      class="px-2 py-0.5 text-sm rounded-md hover:bg-accent/40 hover:text-text-400 transition-colors" 
+      on:click={() => (selectedMode = "ALL")}>All</button
+    >
+    <button
+      class="px-2 py-0.5 text-sm rounded hover:bg-accent/40 hover:text-text-400 transition-colors"
       class:selected={selectedMode === "ANIME"}
-      on:click={() => selectedMode = "ANIME"}
-    >Anime</button>
+      on:click={() => (selectedMode = "ANIME")}>Anime</button
+    >
     <button
-      class="px-2 py-0.5 text-sm rounded-md hover:bg-accent/40 hover:text-text-400 transition-colors" 
+      class="px-2 py-0.5 text-sm rounded hover:bg-accent/40 hover:text-text-400 transition-colors"
       class:selected={selectedMode === "MANGA"}
-      on:click={() => selectedMode = "MANGA"}
-    >Manga</button>
+      on:click={() => (selectedMode = "MANGA")}>Manga</button
+    >
     <button
-      class="px-2 py-0.5 text-sm rounded-md hover:bg-accent/40 hover:text-text-400 transition-colors" 
+      class="px-2 py-0.5 text-sm rounded hover:bg-accent/40 hover:text-text-400 transition-colors"
       class:selected={selectedMode === "LIGHT_NOVEL"}
-      on:click={() => selectedMode = "LIGHT_NOVEL"}
-    >Novel</button>
+      on:click={() => (selectedMode = "LIGHT_NOVEL")}>Novel</button
+    >
   </div>
   {#if $search.fetching}
     <Loader />
@@ -78,22 +98,27 @@
       {#if !$search.data.Page.media?.length}
         <Error warning text="No results found!" />
       {:else}
-        <SearchResults 
-          title="Anime" 
-          results={$search.data.Page.media.filter(m => m.type === MediaType.ANIME)} 
+        <SearchResults
+          title="Anime"
+          results={$search.data.Page.media.filter(
+            (m) => m.type === MediaType.ANIME
+          )}
         />
-        <SearchResults 
-          title="Manga" 
-          results={$search.data.Page.media.filter(m => m.type === MediaType.MANGA && m.format !== MediaFormat.NOVEL)} 
+        <SearchResults
+          title="Manga"
+          results={$search.data.Page.media.filter(
+            (m) => m.type === MediaType.MANGA && m.format !== MediaFormat.NOVEL
+          )}
         />
-        <SearchResults 
-          title="Light Novels" 
-          results={$search.data.Page.media.filter(m => m.format === MediaFormat.NOVEL)} 
+        <SearchResults
+          title="Light Novels"
+          results={$search.data.Page.media.filter(
+            (m) => m.format === MediaFormat.NOVEL
+          )}
         />
       {/if}
     </div>
   {/if}
-
 </div>
 
 <style scoped>
